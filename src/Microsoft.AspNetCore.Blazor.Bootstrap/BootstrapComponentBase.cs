423429e4899657bd4e17c8c77108d3849ed5e52c
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-04-23 12:55:02 +0100

    Mechanism for setting arbitrary HTML attributes, CSS class names, etc. on the Bootstrap component output

--- src/Microsoft.AspNetCore.Blazor.Bootstrap/BootstrapComponentBase.cs
+++ src/Microsoft.AspNetCore.Blazor.Bootstrap/BootstrapComponentBase.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNetCore.Blazor.Components;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Microsoft.AspNetCore.Blazor.Bootstrap
 {
@@ -10,5 +12,37 @@ namespace Microsoft.AspNetCore.Blazor.Bootstrap
     /// </summary>
     public abstract class BootstrapComponentBase : BlazorComponent
     {
+        /// <summary>
+        /// A dictionary holding any parameter name/value pairs that do not match
+        /// properties declared on the component.
+        /// </summary>
+        protected IDictionary<string, object> UnknownParameters { get; }
+            = new Dictionary<string, object>();
+
+        /// <inheritdoc />
+        public override void SetParameters(ParameterCollection parameters)
+        {
+            UnknownParameters.Clear();
+
+            foreach (var param in parameters)
+            {
+                if (TryGetPropertyInfo(param.Name, out var declaredPropertyInfo))
+                {
+                    declaredPropertyInfo.SetValue(this, param.Value);
+                }
+                else
+                {
+                    UnknownParameters[param.Name] = param.Value;
+                }
+            }
+
+            StateHasChanged();
+        }
+
+        private bool TryGetPropertyInfo(string propertyName, out PropertyInfo result)
+        {
+            result = GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+            return result != null;
+        }
     }
 }
