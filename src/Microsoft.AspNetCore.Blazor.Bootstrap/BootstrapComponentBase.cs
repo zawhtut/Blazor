a8ce8701caaad23eedb151a67ac33e4fb07aa701
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-04-23 12:07:30 +0100

    More basics

---
+++ src/Microsoft.AspNetCore.Blazor.Bootstrap/BootstrapComponentBase.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace Microsoft.AspNetCore.Blazor.Bootstrap
+{
+    /// <summary>
+    /// The base class for Blazor Bootstrap components.
+    /// </summary>
+    public abstract class BootstrapComponentBase : BlazorComponent
+    {
+    }
+}
